84f3e81da917a271017320a3d21057459731181e
Prateek M <prateek.machiraju@outlook.com>
2017-11-09 18:50:34 -0500

    Bug fix bc Jaguars are bad

--- src/org/usfirst/frc/team5190/robot/subsystems/JDTSubsystem.java
+++ src/org/usfirst/frc/team5190/robot/subsystems/JDTSubsystem.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team5190.robot.subsystems;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.RobotDrive;
+import org.usfirst.frc.team5190.robot.Robot;
 
 import static org.usfirst.frc.team5190.robot.RobotMap.*;
 
@@ -15,5 +16,11 @@ public class JDTSubsystem extends DTSubsystem
     {
         robotDrive = new RobotDrive(jFrontLeft, jRearLeft, jFrontRight, jRearRight);
         gyro = new AHRS(I2C.Port.kMXP);
+    }
+
+    @Override
+    public void drive()
+    {
+        robotDrive.arcadeDrive(Robot.oi.getJoystick());
     }
 }
